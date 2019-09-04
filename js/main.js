aededf18a0e8362906a7f36570ce0f6517e6e1ea
esimabol <edylber.simancas@outlook.com>
2019-09-03 21:00:34 -0500

    Detectando Eventos del Teclado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 const startingPoint = () => {
-    const canvasElement = document.getElementById('drawing_area');
-    lienzo = canvasElement.getContext('2d');
 
     const drawLine = (color, xi, yi, xf, yf) => {
         lienzo.beginPath();
@@ -10,8 +8,34 @@ const startingPoint = () => {
         lienzo.stroke();
         lienzo.closePath();
     }
+
+    const keyPressed = (e) => {
+        if (e.keyCode === 40) {
+            console.log('Felcha Hacia Abajo');
+        }
+        else if (e.keyCode === 38) {
+            console.log('Felcha Hacia Arriba');
+        }
+        else if (e.keyCode === 39) {
+            console.log('Felcha Hacia la Derecha');
+        }
+        else if (e.keyCode === 37) {
+            console.log('Felcha Hacia la Izquierda');
+        }
+        else {
+            console.log('Utilice las flechas del teclado');
+        }
+        
+    }
+
+
+    let canvasElement = document.getElementById('drawing_area');
+    lienzo = canvasElement.getContext('2d');
+    document.addEventListener('keydown', keyPressed, false);
+
+
     
-    drawLine('blue', 0, 0, 500, 500);
+    
     
     
 
