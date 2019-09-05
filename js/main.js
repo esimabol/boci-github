d9cf235964e3c63dfdaffac1784e6a009f1521c6
esimabol <edylber.simancas@outlook.com>
2019-09-04 20:54:40 -0500

    Dibujando lineas con el teclado

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 const startingPoint = () => {
 
-    const drawLine = (color, xi, yi, xf, yf) => {
+    const drawLine = (color, width, xi, yi, xf, yf) => {
         lienzo.beginPath();
         lienzo.strokeStyle = color;
+        lienzo.lineWidth = width;
         lienzo.moveTo(xi,yi);
         lienzo.lineTo(xf,yf);
         lienzo.stroke();
@@ -10,29 +11,49 @@ const startingPoint = () => {
     }
 
     const keyPressed = (e) => {
-        if (e.keyCode === 40) {
-            console.log('Felcha Hacia Abajo');
-        }
-        else if (e.keyCode === 38) {
-            console.log('Felcha Hacia Arriba');
-        }
-        else if (e.keyCode === 39) {
-            console.log('Felcha Hacia la Derecha');
-        }
-        else if (e.keyCode === 37) {
-            console.log('Felcha Hacia la Izquierda');
-        }
-        else {
-            console.log('Utilice las flechas del teclado');
-        }
-        
-    }
+        const lineColor = 'magenta';
+        const desplazamiento = 10; 
+        const anchoDeLinea = 3;
 
+        switch(e.keyCode) {
+            case teclas.up:
+                drawLine(lineColor, anchoDeLinea, x, y, x, y - desplazamiento);
+                y = y - desplazamiento
+            break;
+            case teclas.down:
+                drawLine(lineColor, anchoDeLinea, x, y, x, y + desplazamiento);
+                y = y + desplazamiento
+            break;
+            case teclas.right:
+                drawLine(lineColor, anchoDeLinea, x, y, x + desplazamiento, y);
+                x = x + desplazamiento
+            break;
+            case teclas.left:
+                drawLine(lineColor, anchoDeLinea, x, y, x - desplazamiento, y);
+                x = x - desplazamiento
+            break;
+
+        }        
+    }
+    
+    // const detectarMouse = (e) =>{
+    //     alert('FUNCIONA CARAJO!');
+    // }
 
     let canvasElement = document.getElementById('drawing_area');
     lienzo = canvasElement.getContext('2d');
     document.addEventListener('keydown', keyPressed, false);
+    // document.addEventListener('mousedown', detectarMouse , false);
 
+    let x =0;
+    let y =0;
+
+    let teclas = {
+        up:38,
+        down:40,
+        left:37,
+        right:39
+    };
 
     
     
